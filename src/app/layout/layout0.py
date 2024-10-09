f15295d2afafc2ef0591dab2cd96dc38829aeb7f
mriusero <marius.ayrault@outlook.com>
2024-10-09 15:11:37 +0200

    API Key test

--- src/app/layout/layout0.py
+++ src/app/layout/layout0.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import os
 
 def page_0():
     st.markdown('<div class="header">#0 Project Overview_</div>', unsafe_allow_html=True)
     st.text("")
     st.text("Here is an overview of the project")
     st.markdown("---")
+
+    # Récupérer la clé API depuis la variable d'environnement
+    openai_api_key = os.getenv("API_KEY")
+
+    if openai_api_key:
+        st.success(f"Utilisation de la clé API: 'openai_api_key'")
+    else:
+        st.error("Pas de clé API fournie.")
