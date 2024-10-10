4cd8d2a877f1a7575514aeb203421fe06530e552
mriusero <160402415+mriusero@users.noreply.github.com>
2024-10-10 22:32:04 +0200

    Update layout1.py

--- src/app/layout/layout1.py
+++ src/app/layout/layout1.py
@@ -3,14 +3,20 @@ import streamlit as st
 from ..components.utils import initialize_hg_api_key
 from ..features.vectors_store.document_processor import ResearchAssistant
 
+from huggingface_hub import HfApi
+
 def page_1():
     st.markdown('<div class="header">#1 Something_</div>', unsafe_allow_html=True)
     st.text("")
     st.text("Here is the page 2")
     st.markdown("---")
 
+    api = HfApi()
     hg_api_key = initialize_hg_api_key()
-    pdf_urls = ["http://arxiv.org/pdf/2405.20113v2", "http://arxiv.org/pdf/2410.07099v1"]
+    st.write(f"## Connexion test")
+    st.write(api.whoami(hg_api_key))
+    
+    #pdf_urls = ["http://arxiv.org/pdf/2405.20113v2", "http://arxiv.org/pdf/2410.07099v1"]
 
-    agent = ResearchAssistant(hg_api_key)
-    agent.run_assistance(pdf_urls)
+    #agent = ResearchAssistant(hg_api_key)
+    #agent.run_assistance(pdf_urls)
